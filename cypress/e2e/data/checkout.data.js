2a42e0021b197534199ae96fd51651bb3ada758a
davinascott <davinascott0401@gmail.com>
2022-12-06 21:24:33 -0500

    second commit

---
+++ cypress/e2e/data/checkout.data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    information: 
+    [{
+        scenario: 'Missing Required Field - First Name',
+        firstName: '',
+        lastName: 'Prescot',
+        zipcode: '000000'
+    },
+    {
+        scenario: 'Valid Checkout Information',
+        firstName: 'Lily',
+        lastName: 'Prescot',
+        zipcode: '000000'
+    }]
+}
---
+++ cypress/e2e/test/checkout.cy.js
@@ -0,0 +1,70 @@
+import loginPage from '../page/login.page'
+import inventoryPage from '../page/inventory.page'
+import cartProducts from '../data/products.data'
+import shoppingCart from '../page/cart.page'
+
+
+describe('Checkout Flow', () => {
+    before(() => {
+        cy.visit('/')
+        loginPage.login('standard_user','secret_sauce')
+    })
+
+    after(() => {
+        inventoryPage.logout()
+    })
+
+    it.skip('Add a single product to cart', () => {
+        inventoryPage.addToCart(cartProducts.products[0].name)
+        inventoryPage.openCart()
+        cy.url().should('contain', 'cart')
+        cy.get(shoppingCart.cartQuantityBadge).should('have.text','1')
+        cy.get(shoppingCart.cartFirstCartItemName).should('have.text', cartProducts.products[0].name)
+    })
+
+    it('Add multiple product to cart', () => {
+        inventoryPage.addToCart(cartProducts.products[0].name)
+        inventoryPage.addToCart(cartProducts.products[1].name)
+        inventoryPage.openCart()
+        cy.url().should('contain', 'cart')
+        cy.get(shoppingCart.cartQuantityBadge).should('have.text','2')
+        cy.get(shoppingCart.cartFirstCartItemName).should('have.text', cartProducts.products[0].name)
+        cy.get(shoppingCart.cartSecondCartItemName).should('have.text', cartProducts.products[1].name)
+    })
+
+    it('Remove cart item', () => {
+        inventoryPage.addToCart(cartProducts.products[0].name)
+        inventoryPage.addToCart(cartProducts.products[1].name)
+        inventoryPage.openCart()
+        cy.url().should('contain', 'cart')
+        cy.get(shoppingCart.cartQuantityBadge).should('have.text','2')
+        cy.get(shoppingCart.cartFirstCartItemName).should('have.text', cartProducts.products[0].name)
+        cy.get(shoppingCart.cartSecondCartItemName).should('have.text', cartProducts.products[1].name)
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).click();
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).should('not.exist')
+    })
+
+    it('Checkout single cart item', () => {
+        inventoryPage.addToCart(cartProducts.products[0].name)
+        inventoryPage.addToCart(cartProducts.products[1].name)
+        inventoryPage.openCart()
+        cy.url().should('contain', 'cart')
+        cy.get(shoppingCart.cartQuantityBadge).should('have.text','2')
+        cy.get(shoppingCart.cartFirstCartItemName).should('have.text', cartProducts.products[0].name)
+        cy.get(shoppingCart.cartSecondCartItemName).should('have.text', cartProducts.products[1].name)
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).click();
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).should('not.exist')
+    })
+
+    it('Checkout multiple cart item', () => {
+        inventoryPage.addToCart(cartProducts.products[0].name)
+        inventoryPage.addToCart(cartProducts.products[1].name)
+        inventoryPage.openCart()
+        cy.url().should('contain', 'cart')
+        cy.get(shoppingCart.cartQuantityBadge).should('have.text','2')
+        cy.get(shoppingCart.cartFirstCartItemName).should('have.text', cartProducts.products[0].name)
+        cy.get(shoppingCart.cartSecondCartItemName).should('have.text', cartProducts.products[1].name)
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).click();
+        cy.get(shoppingCart.cartFirstItemRemoveBtn).should('not.exist')
+    })
+})
